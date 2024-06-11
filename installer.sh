20a5a43f9990ca3daf7527e78e9d8d84ab8930c9
Magnus Quist <cloudquist@gmail.com>
2024-06-11 18:02:04 +0200

    updated installer

--- installer.sh
+++ installer.sh
@@ -2,14 +2,27 @@
 
 # Variables
 DOCKER_COMPOSE_FILE="docker-compose.yml"
-# ADD GITHUB TOKEN
-# ADD GITHUB USERNAME
-
-# Login to GitHub Container Registry
-docker login ghcr.io -u $GITHUB_USERNAME -p $GITHUB_TOKEN
 
 # Pull images from GitHub Container Registry
-docker-compose -f $DOCKER_COMPOSE_FILE pull
+echo "Pulling Docker images..."
+docker-compose -f "$DOCKER_COMPOSE_FILE" pull
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to pull Docker images."
+    exit 1
+fi
 
 # Run Docker Compose
-docker-compose -f $DOCKER_COMPOSE_FILE up -d
+echo "Starting Docker containers..."
+docker-compose -f "$DOCKER_COMPOSE_FILE" up -d
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to start Docker containers."
+    exit 1
+fi
+
+# Add a delay to ensure services are up (adjust the sleep duration if needed)
+echo "Waiting for services to start..."
+sleep 10
+
+echo "################################################"
+echo "# Dashboard: http://localhost:8080             #"
+echo "################################################"
